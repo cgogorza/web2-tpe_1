1242d9b0e8f5cef4f082b06d156aaa7c748bed4f
Claudia Gogorza <claudiagogorza65@gmail.com>
2023-10-08 18:30:41 -0200

    Se agregan cambios de tabla dinámica y se modifica lectura action

--- router.php
+++ router.php
@@ -4,9 +4,11 @@ require "./app/controllers/auth.controller.php";
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-$action = 'listar'; 
-if (!empty( $_GET['action'])) {
+// lee la acción
+if (!empty($_GET['action'])) {
     $action = $_GET['action'];
+} else {
+    $action = 'listar'; // acción por defecto si no envían
 }
 
 $params = explode('/', $action);
